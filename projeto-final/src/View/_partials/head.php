8c6b77e720890b4950f99c786e83268fa9e93e28
Gustavo Galote <gusgalot3g@gmail.com>
2022-06-17 23:51:31 -0300

    feat: add navbar and container

--- projeto-final/src/View/_partials/head.php
+++ projeto-final/src/View/_partials/head.php
@@ -1,3 +1,7 @@
+<?php
+$url = explode('?', $_SERVER['REQUEST_URI'])[0];
+?>
+
 <!doctype html>
 <html lang="pt-br">
 
@@ -9,3 +13,24 @@
 </head>
 
 <body>
+
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link <?= $url == '/categorias' ? 'active' : '' ?>" href="/categorias">Categorias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?= $url == '/produtos' ? 'active' : '' ?>" href="/produtos">Produtos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-sm">
